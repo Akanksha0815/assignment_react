e0b4894582a0234dd45eed0fbc648ef24e43d486
akanksha0815 <prachi0615@gmail.com>
2020-09-29 22:37:52 +0530

    Updates

---
+++ precache-manifest.f7064d0d8df10d0367c3a777a54e7d57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f944b4e9b578eb129c270abe20de8139",
+    "url": "/assignment_react/index.html"
+  },
+  {
+    "revision": "99dc95a13339e856c5bd",
+    "url": "/assignment_react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "04156d25edb012ad3cea",
+    "url": "/assignment_react/static/js/2.3ce7604c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/assignment_react/static/js/2.3ce7604c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99dc95a13339e856c5bd",
+    "url": "/assignment_react/static/js/main.500643ae.chunk.js"
+  },
+  {
+    "revision": "61551af1845541ec16a7",
+    "url": "/assignment_react/static/js/runtime-main.a4a0b96e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/assignment_react/static/media/logo.5d5d9eef.svg"
+  }
+]);
